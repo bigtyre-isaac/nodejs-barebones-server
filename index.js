f1c37e75148024226e1d9c3f516488d2420295ce
Isaac Osment <isaac@bigtyre.com.au>
2022-04-29 15:06:36 +1000

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const { DateTime } = require('luxon')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/time', (req, res) => {
+  let now = DateTime.now().toISO();
+  res.send(now)
+})
+
+app.listen(port, (err) => {
+  if (err) {
+    console.log(err)
+  }
+  else {
+    console.log(`Example app listening on port ${port}`)
+  }
+})
